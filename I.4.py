ea72ac7e881f78b412570d442dedc10ccc53beef
Jean Dubois <jd-dev@laposte.net>
2020-12-21 17:55:32 +0100

    ajout de turtle

--- I.4.py
+++ I.4.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 # coding:utf-8
-# no imports
+import turtle
+
 
 def f(x):
     return ((2 * x) ** 2 - 12 * x) + 20
 
+
 # number = float(input("Entrez un nombre : "))
 # print(f(number))
 
+turtle.up()
 number = -10
-for loop in range(21):
+turtle.goto(-500, -100)
+turtle.down()
+turtle.forward(1000)
+turtle.up()
+turtle.goto(0, -500)
+turtle.left(90)
+turtle.down()
+turtle.forward(1000)
+turtle.up()
+for loop in range(31):
     print("f({}) = ".format(number), f(number))
+    turtle.goto(number, f(number) - 100)
+    turtle.down()
     number += 1
-    
+
+print("Terminé, cliquez pour quitter.")
+turtle.exitonclick()
