54a556acb6655f1c2672ede84ce5c5ba33307ae4
Jean Dubois <jd-dev@laposte.net>
2020-12-22 12:16:51 +0100

    amélioration

--- I.4.py
+++ I.4.py
@@ -7,8 +7,12 @@ def f(x):
     return ((2 * x) ** 2 - 12 * x) + 20
 
 
+turtle.setworldcoordinates(-200, 0, 200, 400)
+turtle.hideturtle()
+turtle.speed(0)
+turtle.getscreen().delay(0)
 turtle.up()
-turtle.goto(-500, -200)
+turtle.goto(-500, 0)
 turtle.down()
 turtle.forward(1000)
 turtle.up()
@@ -21,9 +25,12 @@ turtle.up()
 number = -15
 for loop in range(121):
     print("f({}) = ".format(number), f(number))
-    turtle.goto(number * 4, f(number) - 200)
+    turtle.goto(number * 4, f(number))
     turtle.down()
     number += 0.25
 
-print("Terminé, cliquez pour quitter.")
-turtle.exitonclick()
+turtle.up()
+turtle.goto(-190, 0)
+turtle.write("Terminé", font=("Courier", 30, "bold"))
+print("Terminé.")
+turtle.mainloop()
