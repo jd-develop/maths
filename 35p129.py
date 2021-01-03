b86076564e5d46f74c3ea2c8938ad0ddcf831767
Jean Dubois <jd-dev@laposte.net>
2021-01-03 16:10:04 +0100

    Create 35p129.py

---
+++ 35p129.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding:utf-8
+"""
+    f(x)=(x-1)(x+1)+1
+    Calcule f(x) pour x=0;1;2;3;4;5;6;7;8;9.
+"""
+
+x = 0
+while not x == 10:
+    fx = (x - 1) * (x + 1) + 1
+    print("f(" + str(x) + ") = " + str(fx))
+    x += 1
