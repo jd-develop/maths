da6fe7025fd3f240f79a0c961b4efca9681d81ce
Jean Dubois <jd-dev@laposte.net>
2021-10-15 15:17:56 +0200

    added nenuphar program

--- 2nde/2021-10/10-15.py
+++ 2nde/2021-10/10-15.py
@@ -4,6 +4,7 @@ import math
 # -----------------
 # EX 98 P 37
 # -----------------
+print("Exercice 98 page 37")
 
 
 def square_roots(n):
@@ -29,3 +30,23 @@ while s100 < 100:
     s100 = square_roots(numbr)
 
 print("Le plus petit nombre tel que la somme précédemment définie soit supérieure à 100 est", str(numbr))
+# -----------------
+# EX 92 P 37
+# -----------------
+print("Exercice 92 page 37")
+
+
+def nenuphar(a, a_max):
+    """
+    Un nénuphar d'aire a m² double de surface toute les heures. On cherche à quelle heure h son aire dépassera a_max m²
+    """
+    h = 0
+    while a <= a_max:
+        a = 2*a
+        h += 1
+    return h
+
+
+aire = eval(input("Aire de départ du nénuphar en m² : "))  # 5e-4
+aire_max = eval(input("Aire max du nénuphar en m² : "))  # 10
+print(nenuphar(aire, aire_max))
