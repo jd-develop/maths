91e9fbd374f0ab320f26deb057d10dd6be9d3f08
Jean Dubois <jd-dev@laposte.net>
2021-11-24 13:54:48 +0100

    add today work

---
+++ 2nde/2021-11/11-24.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding:utf-8
+# premier nombre n tel que x**n > s
+x, s = eval(input("Entrez x, s avec x>1 : "))
+n, xn = 0, 1
+
+while xn <= s:
+    xn *= x
+    n += 1
+
+print(str(n) + " est le premier nombre n tel que x^n > s")  # x^n > s
