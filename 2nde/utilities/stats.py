91bab83a341242b2b19f421e85b92d875928bca2
Jean Dubois <jd-dev@laposte.net>
2022-05-15 20:13:09 +0200

    Create stats.py

---
+++ 2nde/utilities/stats.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import math
+import statistics
+
+values = []
+coefficients = []
+
+should_continue = True
+should_break = False
+
+while should_continue:
+    value = input("Enter a value or 'end' to end: ")
+    if value == 'end':
+        break
+
+    while not isinstance(value, int):
+        try:
+            value = int(value)
+        except Exception:
+            print("The value must be a number")
+            value = input("Enter a value or 'end' to end: ")
+            if value == 'end':
+                should_break = True
+                break
+    if should_break:
+        break
+
+    coefficient = input(f"Enter the coefficient of the value '{value}': ")
+    while not isinstance(coefficient, int):
+        try:
+            coefficient = int(coefficient)
+        except Exception:
+            print("The coefficient must be a number")
+            coefficient = input(f"Enter the coefficient of the value '{value}': ")
+
+    if value not in values:
+        values.append(value)
+        coefficients.append(coefficient)
+    else:
+        coefficients[values.index(value)] += coefficient
+
+
+values_ = []
+for index, value in enumerate(values):
+    for loop in range(coefficients[index]):
+        values_.append(value)
+
+values_.sort()
+
+mean = statistics.mean(values_)
+
+quartiles = statistics.quantiles(values_, n=4)
+first_quartile = quartiles[0]
+median = statistics.median(values_)
+third_quartile = quartiles[2]
+
+harmonic_mean = statistics.harmonic_mean(values_)
+geometric_mean = statistics.geometric_mean(values_)
+
+variance = sum([(value-mean)**2 for value in values_]) / len(values_)
+standard_deviation = math.sqrt(variance)
+
+absolute_differences = [abs(value-mean) for value in values_]
+mean_absolute_difference = sum(absolute_differences) / len(absolute_differences)
+median_absolute_difference = statistics.quantiles(absolute_differences, n=2)[0]
+
+scope = max(values_) - min(values_)
+
+mode = statistics.mode(values_)
+
+print(f"Mean / Moyenne ................................. : {mean}\n")
+
+print(f"Harmonic mean / Moyenne harmonique ............. : {harmonic_mean}")
+print(f"Geometric mean / Moyenne géométrique ........... : {geometric_mean}\n")
+
+print(f"First quartile / Premier quartile .............. : {first_quartile}")
+print(f"Median / Médiane ............................... : {median}")
+print(f"Third quartile / Troisième quartile ............ : {third_quartile}\n")
+
+print(f"Variance ....................................... : {variance}")
+print(f"Standard deviation / Écart-type ................ : {standard_deviation}\n")
+
+print(f"Mean absolute difference / Écart moyen absolu .. : {mean_absolute_difference}")
+print(f"Median absolute difference / Écart médian absolu : {median_absolute_difference}\n")
+
+print(f"Scope / Étendue ................................ : {scope}\n")
+
+print(f"Mode ........................................... : {mode}")
