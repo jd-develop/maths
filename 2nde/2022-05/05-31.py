2d38062e3495b0f082291bd0983706c73c3254e8
Jean Dubois <jd-dev@laposte.net>
2022-05-31 08:55:20 +0200

    add ex37p342

--- 2nde/2022-05/05-31.py
+++ 2nde/2022-05/05-31.py
@@ -17,3 +17,18 @@ def rep_chat(n):
 
 for _ in range(10):
     print(rep_chat(1000))
+
+
+# ex 37p342
+def coin(n: int):
+    s = 0
+    for loop in range(n):
+        if random() < 0.5:
+            s += 1
+    return s / n
+
+
+print(coin(10))
+print(coin(1000))
+print(coin(10000))
+print(coin(1000000))
