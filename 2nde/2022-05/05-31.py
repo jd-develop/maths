440bfc93aef840419929a17ebcab9eb6c1be3a07
Jean Dubois <jd-dev@laposte.net>
2022-05-31 08:27:03 +0200

    add 34p342

---
+++ 2nde/2022-05/05-31.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# coding:utf-8
+from random import random
+
+
+# ex 34p342
+def chat():
+    return random() < 0.28
+
+
+def rep_chat(n):
+    s = 0
+    for loop in range(n):
+        s += int(chat())
+    return s
+
+
+for _ in range(10):
+    print(rep_chat(100))
