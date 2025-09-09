7391f53b3f60b26447b2470aa6f4210318f04aa0
Jean Dubois <jd-dev@laposte.net>
2025-09-09 21:47:19 +0100

    DM1: question 20

---
+++ MPI/dm1q20.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+def frac_simple(x: float, accu: list[int], n: int = 3) -> list[int]:
+     if n == 0:
+         return [*accu]
+     a_i = int(x)
+     y_i = x-a_i
+     if y_i==0:
+         return [*accu, a_i]
+     ap_ip1 = 1/y_i
+     return frac_simple(ap_ip1, [*accu, a_i], n-1)
+
+
+def frac_inv(frac: list[int]) -> int | float:
+    if len(frac) == 1: return frac[0]
+    return frac[0]+1/(frac_inv(frac[1:]))
+
+
